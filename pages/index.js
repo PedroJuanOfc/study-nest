d0e7cbfcf7fee043f3ea07b7bd21bd9deef57bb0
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-10-23 20:14:07 -0300

    Criando a página básica de 'Em construção'

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <>
+      <h1>Em construção...</h1>
+      <p>
+        Estamos em processo de construção do nosso ambiente de troca de
+        conhecimentos!
+        <br />
+        Caso tenha interesse em acompanhar o desenvolvimento, ou até mesmo
+        contribuir, encontre o repositório{" "}
+        <a href="https://github.com/PedroJuanOfc/study-nest">aqui</a>!
+      </p>
+    </>
+  );
+}
+
+export default Home;
